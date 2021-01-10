4ebab490bba40738c84f31ae5c20c68f7d2fd734
AbdallahElsayed73 <abdallah.arafa@student.guc.edu.eg>
2021-01-10 03:32:26 +0200

    jlnlk

---
+++ GUCera/InstructorHome.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GUCera
+{
+    public partial class InstructorHome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void courses_Click(object sender, ImageClickEventArgs e)
+        {
+            Session["course"] = 0;
+            Response.Redirect("AddCourse.aspx");
+        }
+
+        protected void promocodes_Click(object sender, ImageClickEventArgs e)
+        {
+            Session["assignment"] = 0;
+            Response.Redirect("AddAssignment.aspx");
+
+        }
+    }
+}
